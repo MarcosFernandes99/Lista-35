898495f363205a252146fb071bd3ce4b7fbbc6d5
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-10-13 22:50:05 -0300

    Exercicio 2 finalizado

--- lista35.js
+++ lista35.js
@@ -1,1 +1,16 @@
 // BORA
+console.log(TransformarPalavra("coDIGo"));
+function TransformarPalavra(palavra) {
+    var maiusculas = /[A-Z]/g;
+    var minusculas = /[a-z]/g;
+    var contadorMa = palavra.match(maiusculas);
+    var contadorMi = palavra.match(minusculas);
+    console.log(contadorMa);
+    console.log(contadorMi);
+    if (contadorMa.length > contadorMi.length) {
+        return palavra.toUpperCase();
+    }
+    else {
+        return palavra.toLowerCase();
+    }
+}
--- lista35.ts
+++ lista35.ts
@@ -1,1 +1,23 @@
 // BORA
+
+console.log(TransformarPalavra("coDIGo"))
+
+
+function TransformarPalavra(palavra: string):string {
+    let maiusculas : RegExp = /[A-Z]/g
+    let minusculas : RegExp = /[a-z]/g
+
+    let contadorMa: string[] | null = palavra.match(maiusculas)
+    let contadorMi: string[] | null = palavra.match(minusculas)
+
+    console.log(contadorMa)
+    console.log(contadorMi)
+
+    if(contadorMa.length > contadorMi.length){
+        return palavra.toUpperCase()
+    }
+    else{
+        return palavra.toLowerCase()
+    }
+
+}
